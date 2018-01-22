fd5e0cd7914dfb392f8f67149ae1af6fa23e076e
Douglas Carlos <dnevescarlos@gmail.com>
2018-01-21 23:36:47 -0200

    create dashboard component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,22 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { HeroesComponent } from './heroes';
+import { DashboardComponent }   from './dashboard';
 
 const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/dashboard',
+    pathMatch: 'full'
+  },
 	{ 
 		path: 'heroes', 
 		component: HeroesComponent
-	}
+	},
+  { 
+    path: 'dashboard', 
+    component: DashboardComponent
+  }
 ];
 
 @NgModule({
